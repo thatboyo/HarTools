75d25f3d0f76e3eeedf8da99c1ba893e7ba10672
HarryJarry <148587892+HarryJarry1@users.noreply.github.com>
2025-05-06 19:31:12 -0400

    org stuff

--- autoxss.py
+++ autoxss.py
@@ -7,8 +7,8 @@ def read_and_encode(path):
 
 # Encode the HTML and JS files
 entry_b64 = read_and_encode("entry.html")
-index_html_b64 = read_and_encode("index.html")
-index_js_b64 = read_and_encode("index.js")
+index_html_b64 = read_and_encode("payloads/index.html")
+index_js_b64 = read_and_encode("payloads/index.js")
 
 # Load the xss.js file
 with open("xss.js", "r", encoding="utf-8") as f:
@@ -20,5 +20,5 @@ js_contents = js_contents.replace("putindex.htmlcontentshere", index_html_b64)
 js_contents = js_contents.replace("putindex.jscontentshere", index_js_b64)
 
 # Save the modified xss.js
-with open("xss.js", "w", encoding="utf-8") as f:
+with open("autoxss.js", "w", encoding="utf-8") as f:
     f.write(js_contents)
